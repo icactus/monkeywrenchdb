a057766b67ba2625c8bc1e640316e2fc072e0cac
Isaac Trapkus <icactusmusic@gmail.com>
2023-10-28 01:24:02 -0400

    fixed get-new-instrument offset and metric

--- get_new_instrument_data.php
+++ get_new_instrument_data.php
@@ -19,10 +19,20 @@ $stmt = $conn->prepare("SELECT metric_arr.*, r.recording_id, r.piece_id, r.youtu
                         CASE 
                             WHEN metric_arr.measures_version = 1 THEN r.offset_js
                             WHEN metric_arr.measures_version = 2 THEN r.offset_js2
+                            WHEN metric_arr.measures_version = 3 THEN r.offset_js_pt1
+                            WHEN metric_arr.measures_version = 4 THEN r.offset_js_pt2
+                            WHEN metric_arr.measures_version = 5 THEN r.offset_js_pt3
+                            WHEN metric_arr.measures_version = 6 THEN r.offset_js_pt4
+                            WHEN metric_arr.measures_version = 7 THEN r.offset_js_pt5
                         END AS offset_js,
                         CASE 
                             WHEN metric_arr.measures_version = 1 THEN r.times_arr_data
                             WHEN metric_arr.measures_version = 2 THEN r.times_arr_data_2
+                            WHEN metric_arr.measures_version = 3 THEN r.times_arr_data_pt1
+                            WHEN metric_arr.measures_version = 4 THEN r.times_arr_data_pt2
+                            WHEN metric_arr.measures_version = 5 THEN r.times_arr_data_pt3
+                            WHEN metric_arr.measures_version = 6 THEN r.times_arr_data_pt4
+                            WHEN metric_arr.measures_version = 7 THEN r.times_arr_data_pt5
                         END AS times_arr_data
                         FROM metric_arr
                         JOIN pieces ON metric_arr.piece_id = pieces.piece_id
