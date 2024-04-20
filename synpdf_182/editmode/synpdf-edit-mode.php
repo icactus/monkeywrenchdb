8a75e839a1b6c941d99be4fccc98308483bee239
Isaac Trapkus <itrapkus@gmail.com>
2024-04-20 13:05:45 -0400

    test fix php locs

--- synpdf_182/editmode/synpdf-edit-mode.php
+++ synpdf_182/editmode/synpdf-edit-mode.php
@@ -134,6 +134,7 @@ sidecontent {
     </style>
 </head>
 <body>
+<?php include "../../phpfiles/get_pieces_and_composers.php"; ?>
 <div id="tooltip"></div><!--for mouse x position element-->
 
 <!-- <div class="main-container"> -->
