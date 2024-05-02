821db0c364131762df9d7986bb6f88b988ffdbe1
Isaac Trapkus <itrapkus@gmail.com>
2024-05-02 18:03:35 -0400

    change pdf paths for online editmode

--- synpdf_182/editmode/synpdf-edit-mode.js
+++ synpdf_182/editmode/synpdf-edit-mode.js
@@ -2605,7 +2605,8 @@ function evalPreload$$module$synpdf(a) {
         b = JSON.parse(b);
         switch (a) {
             case "pdf_file":
-                pdf_file$$module$synpdf = b;
+                pdf_file$$module$synpdf = "../pdfs/" + b;
+                console.log(pdf_file$$module$synpdf);
                 break;
             case "media_dir":
                 media_dir$$module$synpdf = b;
