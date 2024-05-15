723d067f028704de4d1751d89a1afc7b140b363f
Isaac Trapkus <itrapkus@gmail.com>
2024-05-14 20:40:19 -0400

    fixed adjustTime2

--- synpdf_182/editmode/edit-mode-tools.js
+++ synpdf_182/editmode/edit-mode-tools.js
@@ -558,7 +558,7 @@ addNewMetricForm.addEventListener("submit", function(event) {
 });
 
 //Allows user to manually mass correct timing across a range - adjustTimeValues(deTijden$$module$synpdf, 5, 10, 0.5);
-const adjustTimeValues = (startIndex, endIndex, timeShift) => (startIndex < 0 || endIndex >= deTijden$$module$synpdf.length || startIndex > endIndex) ? console.error("Invalid indices");
+const adjustTimeValues = (startIndex, endIndex, timeShift) => (startIndex < 0 || endIndex >= deTijden$$module$synpdf.length || startIndex > endIndex) ? console.error("Invalid indices") : array.slice(startIndex, endIndex + 1).forEach(item => item.t += timeShift);
 
 document.addEventListener('DOMContentLoaded', function() {
     var form = document.getElementById('addnewrecordingform');
