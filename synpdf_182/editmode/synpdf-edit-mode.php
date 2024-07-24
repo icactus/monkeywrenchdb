c3150aa10de678cdc66a5ba0afcb2d11480fb4a3
Isaac Trapkus <itrapkus@gmail.com>
2024-07-24 17:22:49 -0400

    rev number

--- synpdf_182/editmode/synpdf-edit-mode.php
+++ synpdf_182/editmode/synpdf-edit-mode.php
@@ -10,7 +10,7 @@
     <script src="jquery.min.js"></script>
     <script src="pdf.min.js"></script>
 
-    <script src="synpdf-edit-mode.js?v=14"></script>
+    <script src="synpdf-edit-mode.js?v=15"></script>
     <style>
 html { width:100%; height:100%; margin: 0px; padding: 0px; background:white/*#e3f7fe*/; }
 body { width: 100%; height: 100%; margin: 0px; padding: 0px; background:white/*#e3f7fe*/; transition: filter 0.5s; }
