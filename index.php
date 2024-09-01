c6bab90d4513feb868e3046c91396ca1383600f8
Isaac Trapkus <itrapkus@gmail.com>
2024-08-31 22:25:54 -0400

    update version nums

--- index.php
+++ index.php
@@ -183,6 +183,6 @@
 
     <script src="pdf.min.js"></script>
     <script src="stripped-synpdf.js?v=13"></script>
-    <script src="stripped-synpdf-extras.js?v=19"></script>
+    <script src="stripped-synpdf-extras.js?v=20"></script>
 </body>
 </html>
