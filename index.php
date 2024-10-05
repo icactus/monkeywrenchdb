3e77a1b0ffa8a8d9316e2cdfe19b54377d42ed72
Isaac Trapkus <itrapkus@gmail.com>
2024-10-05 10:45:08 -0400

    recording ordering2

--- index.php
+++ index.php
@@ -183,6 +183,6 @@
 
     <script src="pdf.min.js"></script>
     <script src="stripped-synpdf.js?v=14"></script>
-    <script src="stripped-synpdf-extras.js?v=22"></script>
+    <script src="stripped-synpdf-extras.js?v=23"></script>
 </body>
 </html>
