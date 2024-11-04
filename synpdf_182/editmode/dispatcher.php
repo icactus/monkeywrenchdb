e439713fbf6bdf44431cab28c6155857e31c934a
Isaac Trapkus <itrapkus@gmail.com>
2024-11-04 14:16:05 -0500

    null option for solo-inst-id3

--- synpdf_182/editmode/dispatcher.php
+++ synpdf_182/editmode/dispatcher.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Prevent direct script access via the dispatcher
 if (!isset($_POST['action'])) {
     echo 'No action specified';
