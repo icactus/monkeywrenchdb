a47cbcb576822cb8e38e4a376cefb83f60abc53b
Isaac Trapkus <itrapkus@gmail.com>
2025-01-01 17:24:48 -0500

    disable keyboard youtube player controls2

--- index.php
+++ index.php
@@ -181,7 +181,7 @@
             </div>
         </section3>
 
-    <script src="stripped-synpdf.js?v=35"></script>
+    <script src="stripped-synpdf.js?v=36"></script>
     <script src="stripped-synpdf-extras.js?v=25"></script>
 </body>
 </html>
