d7690417ac54aaf296dd522ed49062f2053367ca
Isaac Trapkus <itrapkus@gmail.com>
2025-01-01 22:16:42 -0500

    only 4 pages at a time for mobile

--- index.php
+++ index.php
@@ -181,7 +181,7 @@
             </div>
         </section3>
 
-    <script src="stripped-synpdf.js?v=50"></script>
+    <script src="stripped-synpdf.js?v=51"></script>
     <script src="stripped-synpdf-extras.js?v=25"></script>
 </body>
 </html>
