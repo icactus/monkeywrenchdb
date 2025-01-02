c48bc9b2c7ae680486ade65f81b2c6e1c9fd3c9f
Isaac Trapkus <itrapkus@gmail.com>
2025-01-01 20:07:12 -0500

    changed render scale3

--- index.php
+++ index.php
@@ -181,7 +181,7 @@
             </div>
         </section3>
 
-    <script src="stripped-synpdf.js?v=41"></script>
+    <script src="stripped-synpdf.js?v=42"></script>
     <script src="stripped-synpdf-extras.js?v=25"></script>
 </body>
 </html>
