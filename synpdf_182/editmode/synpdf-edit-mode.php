f4997c1639c5411c22538355aebff1ee465427a4
Isaac Trapkus <itrapkus@gmail.com>
2025-01-05 21:48:28 -0500

    one line load score

--- synpdf_182/editmode/synpdf-edit-mode.php
+++ synpdf_182/editmode/synpdf-edit-mode.php
@@ -681,6 +681,7 @@
                                     <option value="<?php echo $pieceId; ?>"><?php echo $pieceName; ?></option>
                                 <?php endforeach; ?>
                             </select>
+                            <button type="button">Load</button>
                         </div>
                     </form>
                     <form class="inputform" id="addnewrecordingform" method="POST">
