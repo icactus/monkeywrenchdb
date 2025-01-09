30549d7cdac62a7ceadf58cec34173f1343a0984
Isaac Trapkus <itrapkus@gmail.com>
2025-01-09 13:53:06 -0500

    remove headings change part/recording

--- index.php
+++ index.php
@@ -206,7 +206,7 @@
     </section3>
 
     <script src="stripped-synpdf.js?v=60"></script>
-    <script src="stripped-synpdf-extras.js?v=27"></script>
+    <script src="stripped-synpdf-extras.js?v=28"></script>
 </body>
 
 </html>
