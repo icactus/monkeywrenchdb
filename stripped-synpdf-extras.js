857329d3db64685b7c7e299a5416dee5f5db0325
Isaac Trapkus <itrapkus@gmail.com>
2025-01-26 14:14:03 -0500

    piece label on recordings tab

--- stripped-synpdf-extras.js
+++ stripped-synpdf-extras.js
@@ -312,7 +312,7 @@ $('#pieces-container').on('click', '.pieces-link', function(event) {
 
     // Clear out old recordings
     $('#recordings-container').empty();
-    $('#recordings-container').prepend($(this).text());
+    $('#recordings-container').prepend($('<h2>').text($(this).text()));
     const pieceId = $(this).data('piece-id');
     const instrumentIds = $(this).data('instrument-id').toString();
     const clickedLink = $(this);
