14d2b19b908d590be87a8db468b7de961ec2a1dd
Isaac Trapkus <itrapkus@gmail.com>
2025-01-26 13:13:36 -0500

    fix parts/score wording

--- index.php
+++ index.php
@@ -258,7 +258,7 @@
     </section3>
 
     <script src="stripped-synpdf.js?v=60"></script>
-    <script src="stripped-synpdf-extras.js?v=34"></script>
+    <script src="stripped-synpdf-extras.js?v=35"></script>
 </body>
 
 </html>
