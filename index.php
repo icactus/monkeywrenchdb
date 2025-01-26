f4bb8103a47cfb8b1bc7e839f7ce3e2669c26800
Isaac Trapkus <itrapkus@gmail.com>
2025-01-26 14:03:04 -0500

    piece label on recordings tab

--- index.php
+++ index.php
@@ -258,7 +258,7 @@
     </section3>
 
     <script src="stripped-synpdf.js?v=60"></script>
-    <script src="stripped-synpdf-extras.js?v=38"></script>
+    <script src="stripped-synpdf-extras.js?v=39"></script>
 </body>
 
 </html>
