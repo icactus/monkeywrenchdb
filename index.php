654f6c4ce6f3ee6edcb9e4ea8612e84fdd46763c
Isaac Trapkus <itrapkus@gmail.com>
2025-01-26 11:13:59 -0500

    instrument selected now appears at top of pieces

--- index.php
+++ index.php
@@ -213,7 +213,7 @@
     </section3>
 
     <script src="stripped-synpdf.js?v=60"></script>
-    <script src="stripped-synpdf-extras.js?v=32"></script>
+    <script src="stripped-synpdf-extras.js?v=33"></script>
 </body>
 
 </html>
