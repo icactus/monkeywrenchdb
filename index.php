dc549ca7d8af8e02c5be535fcc733d0a57079977
Isaac Trapkus <itrapkus@gmail.com>
2025-01-26 20:49:27 -0500

    header text color

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <link rel="stylesheet" href="fonts.css" />
-    <link rel="stylesheet" type="text/css" href="stripped-synpdf-styles.css?v=23" />
+    <link rel="stylesheet" type="text/css" href="stripped-synpdf-styles.css?v=24" />
     <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
     <script src="jquery.min.js"></script>
     <title>monkey wrench</title>
