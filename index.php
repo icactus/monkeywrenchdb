7456b9a8c89d8803f313e7ece964cd4bf183cb9d
Isaac Trapkus <itrapkus@gmail.com>
2025-02-08 16:17:09 -0500

    updating rev#

--- index.php
+++ index.php
@@ -224,7 +224,7 @@
     </section3>
 
     <script src="stripped-synpdf.js?v=62"></script>
-    <script src="stripped-synpdf-extras.js?v=47"></script>
+    <script src="stripped-synpdf-extras.js?v=48"></script>
 </body>
 
 </html>
