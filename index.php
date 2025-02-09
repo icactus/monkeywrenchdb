358861455b8e86dfa6fe5b921499cb4548095dfb
Isaac Trapkus <itrapkus@gmail.com>
2025-02-09 16:42:33 -0500

    troubleshooting popstate2

--- index.php
+++ index.php
@@ -224,7 +224,7 @@
     </section3>
 
     <script src="stripped-synpdf.js?v=63"></script>
-    <script src="stripped-synpdf-extras.js?v=52"></script>
+    <script src="stripped-synpdf-extras.js?v=53"></script>
 </body>
 
 </html>
