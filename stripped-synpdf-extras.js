d52ae0de0fe5b70ac5bee4923a08535564a903d9
Isaac Trapkus <itrapkus@gmail.com>
2025-02-25 16:56:08 -0500

    move darkmode button

--- stripped-synpdf-extras.js
+++ stripped-synpdf-extras.js
@@ -559,13 +559,22 @@ function sendVarToSynpdf(recordingFullData) {
 }
 
 
-document.getElementById('invert-button').addEventListener('click', function() {
-    document.body.classList.toggle('inverted');
-    this.textContent = document.body.classList.contains('inverted') ? 'Light Mode' : 'Dark Mode';
+function addInvertButtonListener() {
+    const invertButton = document.getElementById('invert-button');
+    if (invertButton) {
+        invertButton.addEventListener('click', function() {
+            document.body.classList.toggle('inverted');
+            this.textContent = document.body.classList.contains('inverted') ? 'Light Mode' : 'Dark Mode';
 
-    var img = document.getElementById('monkey-logo');
-    img.src = document.body.classList.contains('inverted') ? 'monkeydark.png' : 'monkeywrench-monkey100x100.png';
-});
+            var img = document.getElementById('monkey-logo');
+            if (img) {
+                img.src = document.body.classList.contains('inverted') ? 'monkeydark.png' : 'monkeywrench-monkey100x100.png';
+            }
+        });
+    } else {
+        console.error('Invert button not found in the DOM.');
+    }
+}
 
 function fetchNewInstrument(instrumentData) {
     return new Promise((resolve, reject) => {
