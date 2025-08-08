8ab49c5bf25350dfbc042bbf7482b5f2d08c7ce8
Isaac Trapkus <itrapkus@gmail.com>
2025-08-08 06:00:13 -0400

    rev #s

--- synpdf_182/editmode/synpdf-edit-mode.php
+++ synpdf_182/editmode/synpdf-edit-mode.php
@@ -11,7 +11,7 @@
     <script src="jquery.min.js"></script>
     <script src="pdf.min.js"></script>
 
-    <script src="synpdf-edit-mode.js?v=41"></script>
+    <script src="synpdf-edit-mode.js?v=42"></script>
     <style>
         html {
             width: 100%;
