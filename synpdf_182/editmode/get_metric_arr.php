86848bc38099da1b4c884a23700bb46be6c9d630
Isaac Trapkus <itrapkus@gmail.com>
2025-08-08 10:40:43 -0400

    fixed directory for php credentials

--- synpdf_182/editmode/get_metric_arr.php
+++ synpdf_182/editmode/get_metric_arr.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once '../phpfiles/read_only_user_config.php';
+require_once '../../phpfiles/read_only_user_config.php';
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 if ($conn->connect_error) {
