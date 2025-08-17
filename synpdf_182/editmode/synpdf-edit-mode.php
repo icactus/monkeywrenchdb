97e99dce4a636d43b32d0c5ab9925a7f39c07640
Isaac Trapkus <itrapkus@gmail.com>
2025-08-17 05:04:04 -0400

    forgot keyboard case for x2

--- synpdf_182/editmode/synpdf-edit-mode.php
+++ synpdf_182/editmode/synpdf-edit-mode.php
@@ -11,7 +11,7 @@
     <script src="jquery.min.js"></script>
     <script src="pdf.min.js"></script>
 
-    <script src="synpdf-edit-mode.js?v=54"></script>
+    <script src="synpdf-edit-mode.js?v=55"></script>
     <style>
         html {
             width: 100%;
