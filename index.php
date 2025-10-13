953ca23c020f39062a2f0888746c373abd9b5554
Isaac Trapkus <itrapkus@gmail.com>
2025-10-13 19:50:34 -0400

    change highlight

--- index.php
+++ index.php
@@ -227,7 +227,7 @@
         Link copied to clipboard!
     </div>
     <script src="stripped-synpdf.js?v=175"></script>
-    <script src="stripped-synpdf-extras.js?v=161"></script>
+    <script src="stripped-synpdf-extras.js?v=162"></script>
 </body>
 
 </html>
