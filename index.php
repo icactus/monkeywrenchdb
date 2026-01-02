0aca82ad30536138dd25d2064fc28a4dbadff898
Isaac Trapkus <itrapkus@gmail.com>
2026-01-02 09:44:22 -0500

    rev numbers

--- index.php
+++ index.php
@@ -246,8 +246,8 @@
         style="display: none; position: fixed; bottom: 60px; right: 20px; background: #333; color: #fff; padding: 10px 20px; border-radius: 5px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);">
         Link copied to clipboard!
     </div>
-    <script src="stripped-synpdf.js?v=176"></script>
-    <script src="stripped-synpdf-extras.js?v=162"></script>
+    <script src="stripped-synpdf.js?v=177"></script>
+    <script src="stripped-synpdf-extras.js?v=163"></script>
 </body>
 
 </html>
