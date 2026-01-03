975f70e9a8d3d384a6897c2d240acf64c7f9e235
Isaac Trapkus <itrapkus@gmail.com>
2026-01-02 20:09:08 -0500

    Add session_config.php to git

---
+++ session_config.php
@@ -0,0 +1,20 @@
+// 1 Year in seconds
+$lifetime = 60 * 60 * 24 * 365;
+
+// Server-side timeout (Garbage Collection)
+ini_set('session.gc_maxlifetime', $lifetime);
+
+// Client-side cookie timeout
+session_set_cookie_params([
+'lifetime' => $lifetime,
+'path' => '/',
+'domain' => '', // Default to current domain
+'secure' => true, // Ensure HTTPS
+'httponly' => true,
+'samesite' => 'Lax'
+]);
+
+if (session_status() === PHP_SESSION_NONE) {
+session_start();
+}
+?>
