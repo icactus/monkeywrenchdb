16747c15dbc6c4e448a931a759db9de903231eb3
Isaac Trapkus <itrapkus@gmail.com>
2026-01-03 08:15:56 -0500

    rev num

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (file_exists('session_config.php')) {
         content="width=device-width, initial-scale=1, viewport-fit=cover, maximum-scale=1, user-scalable=no" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <link rel="stylesheet" href="assets/css/fonts.css?v=3" />
-    <link rel="stylesheet" type="text/css" href="assets/css/stripped-synpdf-styles.css?v=73" />
+    <link rel="stylesheet" type="text/css" href="assets/css/stripped-synpdf-styles.css?v=74" />
     <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -273,7 +273,7 @@ if (file_exists('session_config.php')) {
         style="display: none; position: fixed; bottom: 60px; right: 20px; background: #333; color: #fff; padding: 10px 20px; border-radius: 5px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);">
         Link copied to clipboard!
     </div>
-    <script src="stripped-synpdf.js?v=182"></script>
+    <script src="stripped-synpdf.js?v=183"></script>
     <script src="stripped-synpdf-extras.js?v=165"></script>
 </body>
 
