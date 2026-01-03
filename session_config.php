92fb6fcd5dc759f9048fa72fc38490727a619ff6
Isaac Trapkus <itrapkus@gmail.com>
2026-01-02 20:18:35 -0500

    syntax in sessionphp

--- session_config.php
+++ session_config.php
@@ -1,3 +1,4 @@
+<?php
 // 1 Year in seconds
 $lifetime = 60 * 60 * 24 * 365;
 
@@ -6,14 +7,14 @@ ini_set('session.gc_maxlifetime', $lifetime);
 
 // Client-side cookie timeout
 session_set_cookie_params([
-'lifetime' => $lifetime,
-'path' => '/',
-'domain' => '', // Default to current domain
-'secure' => true, // Ensure HTTPS
-'httponly' => true,
-'samesite' => 'Lax'
+    'lifetime' => $lifetime,
+    'path' => '/',
+    'domain' => '', // Default to current domain
+    'secure' => true, // Ensure HTTPS
+    'httponly' => true,
+    'samesite' => 'Lax'
 ]);
 
 if (session_status() === PHP_SESSION_NONE) {
-session_start();
+    session_start();
 }
