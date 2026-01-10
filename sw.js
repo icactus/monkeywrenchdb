b4e39f2612929872c3a58659788a988e19da610b
Isaac Trapkus <itrapkus@gmail.com>
2026-01-10 15:56:30 -0500

    padding creating white strip - removed

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Version 2 - Online-only, always fetch fresh content
 // Bump this version number to force update on all clients
 
-const SW_VERSION = 34;
+const SW_VERSION = 35;
 
 // Install event - activate immediately
 self.addEventListener('install', (event) => {
