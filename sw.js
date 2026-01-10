54833f45e17c874828471d9d9bb9278f458a1092
Isaac Trapkus <itrapkus@gmail.com>
2026-01-10 15:58:41 -0500

    app top padding fix 2

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Version 2 - Online-only, always fetch fresh content
 // Bump this version number to force update on all clients
 
-const SW_VERSION = 35;
+const SW_VERSION = 36;
 
 // Install event - activate immediately
 self.addEventListener('install', (event) => {
