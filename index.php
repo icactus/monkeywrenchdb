513c5e401aca99afdc0ff0f5341bce7adcc05cca
Isaac Trapkus <itrapkus@gmail.com>
2026-01-10 16:20:58 -0500

    top bar back to black

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $is_share_link = isset($_GET['share']);
         content="width=device-width, initial-scale=1, viewport-fit=cover, maximum-scale=1, user-scalable=no" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-    <meta name="theme-color" content="#f7d155" />
+    <meta name="theme-color" content="#000000" />
     <link rel="manifest" href="/manifest.json" />
     <link rel="apple-touch-icon" href="/assets/img/pwa-icon-192-v3.png" />
     <link rel="stylesheet" href="assets/css/fonts.css?v=3" />
