1958476b3358d1c420cf2442a138a8c779615672
Isaac Trapkus <itrapkus@gmail.com>
2026-01-10 16:04:36 -0500

    background fussing at top for mobile

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Version 2 - Online-only, always fetch fresh content
 // Bump this version number to force update on all clients
 
-const SW_VERSION = 37;
+const SW_VERSION = 38;
 
 // Install event - activate immediately
 self.addEventListener('install', (event) => {
