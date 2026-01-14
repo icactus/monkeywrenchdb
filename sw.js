96568dcdfddbca70b625ca8efaf82776e8de5fa5
Isaac Trapkus <itrapkus@gmail.com>
2026-01-13 22:43:05 -0500

    css rev num

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Version 2 - Online-only, always fetch fresh content
 // Bump this version number to force update on all clients
 
-const SW_VERSION = 64;
+const SW_VERSION = 65;
 
 // Install event - activate immediately
 self.addEventListener('install', (event) => {
