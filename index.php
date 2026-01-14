16fe0b8b6c4f42cdd63b21d014ab257c638daeaf
Isaac Trapkus <itrapkus@gmail.com>
2026-01-13 22:40:29 -0500

    share notification zindex

--- index.php
+++ index.php
@@ -596,7 +596,7 @@ $is_share_link = isset($_GET['share']);
 
     <!-- Notification Element -->
     <div id="notification"
-        style="display: none; position: fixed; bottom: 60px; right: 20px; background: #333; color: #fff; padding: 10px 20px; border-radius: 5px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);">
+        style="display: none; position: fixed; bottom: 60px; right: 20px; background: #333; color: #fff; padding: 10px 20px; border-radius: 5px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); z-index: 10001;">
         Link copied to clipboard!
     </div>
     <script src="stripped-synpdf.js?v=241"></script>
