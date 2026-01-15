368848baaf78d7484791e1b23246e302bc358194
Isaac Trapkus <itrapkus@gmail.com>
2026-01-15 15:24:30 -0500

    user stuff under logo

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Version 2 - Online-only, always fetch fresh content
 // Bump this version number to force update on all clients
 
-const SW_VERSION = 99;
+const SW_VERSION = 100;
 
 // Install event - activate immediately
 self.addEventListener('install', (event) => {
