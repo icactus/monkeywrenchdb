510c575ec138f6c54aa3747daf3d2430ff7a799e
Isaac Trapkus <itrapkus@gmail.com>
2026-01-15 14:43:26 -0500

    modal positions again

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Version 2 - Online-only, always fetch fresh content
 // Bump this version number to force update on all clients
 
-const SW_VERSION = 92;
+const SW_VERSION = 93;
 
 // Install event - activate immediately
 self.addEventListener('install', (event) => {
