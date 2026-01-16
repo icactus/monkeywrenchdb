7f88128f78f79954b9101d042342b5ead690d2ff
Isaac Trapkus <itrapkus@gmail.com>
2026-01-15 19:13:09 -0500

    user dropdown text color hover

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Version 2 - Online-only, always fetch fresh content
 // Bump this version number to force update on all clients
 
-const SW_VERSION = 106;
+const SW_VERSION = 107;
 
 // Install event - activate immediately
 self.addEventListener('install', (event) => {
