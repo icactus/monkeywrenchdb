47f2fa60c4dcfeaaa02c330347e83b490a115b90
Isaac Trapkus <itrapkus@gmail.com>
2026-01-19 23:09:02 -0500

    Refactor: Move annotation-layer.js to js/ folder and cleanup stripped-synpdf.js

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $is_share_link = isset($_GET['share']);
         rel="stylesheet">
     <script src="jquery.min.js"></script>
     <?php if (isset($_SESSION['user_id']) || $is_share_link): ?>
-        <script src="annotation-layer.js?v=100"></script>
+        <script src="js/annotation-layer.js?v=100"></script>
     <?php endif; ?>
     <script>
         window.loggedInUserId = <?php echo isset($_SESSION['user_id']) ? (int) $_SESSION['user_id'] : 'null'; ?>;
