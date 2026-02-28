eecfa3e842a7a05f4b88b554d1b038dcb33466d6
Isaac Trapkus <itrapkus@gmail.com>
2026-02-28 16:34:44 -0500

    Bump version for cache busting

--- preview-editor.php
+++ preview-editor.php
@@ -754,7 +754,7 @@ $is_share_link = isset($_GET['share']);
             });
         }
     </script>
-    <script src="synpdf-editor-extension.js?v=5"></script>
+    <script src="synpdf-editor-extension.js?v=6"></script>
 </body>
 
 </html>
