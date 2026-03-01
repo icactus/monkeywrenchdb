f3418eee946c84f2f44691e452f9c3d61a55de9d
Isaac Trapkus <itrapkus@gmail.com>
2026-03-01 18:54:22 -0500

    better dtw and metronome for preview-editor

--- preview-editor.php
+++ preview-editor.php
@@ -754,7 +754,7 @@ $is_share_link = isset($_GET['share']);
             });
         }
     </script>
-    <script src="synpdf-editor-extension.js?v=11"></script>
+    <script src="synpdf-editor-extension.js?v=12"></script>
 </body>
 
 </html>
