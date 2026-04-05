7c7186e8bb02a3c61e49047dedfc2c4931dc033c
Isaac Trapkus <itrapkus@gmail.com>
2026-04-04 21:52:32 -0400

    Fit merged piano systems with sparse geometry

--- synpdf_182/editmode/edit-mode-tools.js
+++ synpdf_182/editmode/edit-mode-tools.js
@@ -1091,10 +1091,33 @@ function runPagePianoGeometryOnly(options) {
         return false;
     }
 
-    var normalizedChanged = normalizePageToPianoSystems(pageData, pageImageData);
-    if (!normalizedChanged) {
-        fitPianoSystemsOnPage(pageData, pageImageData);
+    normalizePageToPianoSystems(pageData, pageImageData);
+
+    let pixelData = pageImageData.pixelData;
+    let stride = pageImageData.stride;
+    let width = pageImageData.width;
+    if (!pixelData || pixelData.length === 0) {
+        if (!options.suppressAlerts) alert('Pixel data extraction failed. Please reload the page.');
+        return false;
     }
+
+    const fittedSystems = JSON.parse(JSON.stringify(pageData.cxs));
+    fittedSystems.forEach(function (system, index) {
+        applyExistingBoundaryXs(system, pageData.bxs && pageData.bxs[index]);
+    });
+
+    pageData.cxs = fittedSystems.map(function (system, index) {
+        if (!Array.isArray(system.cs) || system.cs.length !== 2) {
+            return system;
+        }
+        var renderGeometry = BarlineDetectV2.buildRenderGeometry(system, pixelData, stride, width);
+        if (!renderGeometry) {
+            return system;
+        }
+        return applyRenderGeometryToSystem(system, renderGeometry, {
+            fixedXs: getSystemBoundaryXs(system, pageData.bxs && pageData.bxs[index])
+        });
+    });
     deMetriek$$module$synpdf[pagenum] = pageData;
 
     if (!persistMetricData()) {
